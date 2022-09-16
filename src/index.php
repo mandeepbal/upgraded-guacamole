f0a7e672975a230a616b80bab135726a354dd8ae
Mandeep Bal <3278289+mandeepbal@users.noreply.github.com>
2022-09-15 23:15:45 -0400

    Update to just Hello World

--- src/index.php
+++ src/index.php
@@ -1,34 +1,7 @@
 <html>
 
-<head>
-  Hello World!
-</head>
-
 <body>
-  <?php
-  $hostname = getenv('HOSTNAME');
-  echo "Container hostname is: $hostname";
-  ?>
-
-
-  <?php
-  $servername = 'database:3306';
-  $username = 'myuser';
-  $password = 'test1234@pl';
-
-  try {
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-  } catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-  }
-  ?>
-
-
+  Hello World!
 </body>
 
 </html>
