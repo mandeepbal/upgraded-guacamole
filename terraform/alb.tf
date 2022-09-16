45c28a6ad6c58bf7075c1248f8a876b0973317d3
Mandeep Bal <3278289+mandeepbal@users.noreply.github.com>
2022-09-15 23:07:40 -0400

    remove https listener since i don't have a cert

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -30,36 +30,8 @@ resource "aws_alb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-/*
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = 443
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-*/
-  default_action {
-    target_group_arn = aws_alb_target_group.lamp_web.id
-    type             = "forward"
-  }
-
-}
-
-/*
-resource "aws_alb_listener" "https" {
-  load_balancer_arn = aws_lb.lamp_web.id
-  port              = 443
-  protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
-//  certificate_arn = var.alb_tls_cert_arn
-
   default_action {
     target_group_arn = aws_alb_target_group.lamp_web.id
     type             = "forward"
   }
 }
-*/
